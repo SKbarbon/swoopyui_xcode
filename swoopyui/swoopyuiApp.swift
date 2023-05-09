ca9eddfac9142f92c51245b870394acad6c6bf85
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-05-09 20:32:50 +0300

    Initial Commit

---
+++ swoopyui/swoopyuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swoopyuiApp.swift
+//  swoopyui
+//
+//  Created by Yousif Aladwani on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct swoopyuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
