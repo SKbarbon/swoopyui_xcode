440ab58bf6669d0707bc4894c33167538fb3db23
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-05-21 12:21:20 +0300

    Update on client side. - Supprt `Text` sizes and bold. - Support VStack, HStack and ZStack.

---
+++ swoopyui/AllViews/TheStackView.swift
@@ -0,0 +1,20 @@
+//
+//  TheStackView.swift
+//  swoopyui
+//
+//  Created by Yousif Aladwani on 21/05/2023.
+//
+
+import SwiftUI
+
+struct TheStackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TheStackView_Previews: PreviewProvider {
+    static var previews: some View {
+        TheStackView()
+    }
+}
