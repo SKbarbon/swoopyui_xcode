0f0d020e9e22d9cb5f704e414b8cab2f1f584d5c
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-05-21 13:58:12 +0300

    Updates: - Support ScrollView.

---
+++ swoopyui/AllViews/TheScrollView.swift
@@ -0,0 +1,20 @@
+//
+//  TheScrollView.swift
+//  swoopyui
+//
+//  Created by Yousif Aladwani on 21/05/2023.
+//
+
+import SwiftUI
+
+struct TheScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TheScrollView_Previews: PreviewProvider {
+    static var previews: some View {
+        TheScrollView()
+    }
+}
