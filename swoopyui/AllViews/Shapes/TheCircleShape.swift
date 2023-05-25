2be468b95ffa2ca8fa5dab9d5589a0f75200c3fb
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-05-25 18:41:24 +0300

    Updates and changes. - Support Spacers. - Support Lists. - Support New shapes: Circle.

---
+++ swoopyui/AllViews/Shapes/TheCircleShape.swift
@@ -0,0 +1,20 @@
+//
+//  TheCircleShape.swift
+//  swoopyui
+//
+//  Created by Yousif Aladwani on 25/05/2023.
+//
+
+import SwiftUI
+
+struct TheCircleShape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TheCircleShape_Previews: PreviewProvider {
+    static var previews: some View {
+        TheCircleShape()
+    }
+}
---
+++ swoopyui/AllViews/TheListView.swift
@@ -0,0 +1,20 @@
+//
+//  TheListView.swift
+//  swoopyui
+//
+//  Created by Yousif Aladwani on 25/05/2023.
+//
+
+import SwiftUI
+
+struct TheListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TheListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TheListView()
+    }
+}
