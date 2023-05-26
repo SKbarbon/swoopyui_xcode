b3d99d1a6d411555f9e9e5f52a04b25310a5a978
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-05-26 14:31:50 +0300

    Support images

---
+++ swoopyui/AllViews/TheImageView.swift
@@ -0,0 +1,20 @@
+//
+//  TheImageView.swift
+//  swoopyui
+//
+//  Created by Yousif Aladwani on 26/05/2023.
+//
+
+import SwiftUI
+
+struct TheImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TheImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        TheImageView()
+    }
+}
