0384fa30cd0653b2ab74a42d2378a5f822da3881
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-06-24 22:12:42 +0300

    Updates and changes - Support webview. - Remove the mainview animation and add new view called `AnimatedView`.

---
+++ swoopyui/AllViews/TheAnimatedView.swift
@@ -0,0 +1,20 @@
+//
+//  TheAnimatedView.swift
+//  swoopyui
+//
+//  Created by Yousif Aladwani on 24/06/2023.
+//
+
+import SwiftUI
+
+struct TheAnimatedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TheAnimatedView_Previews: PreviewProvider {
+    static var previews: some View {
+        TheAnimatedView()
+    }
+}
---
+++ swoopyui/AllViews/TheWebView.swift
@@ -0,0 +1,20 @@
+//
+//  TheWebView.swift
+//  swoopyui
+//
+//  Created by Yousif Aladwani on 24/06/2023.
+//
+
+import SwiftUI
+
+struct TheWebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TheWebView_Previews: PreviewProvider {
+    static var previews: some View {
+        TheWebView()
+    }
+}
